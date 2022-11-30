210d59e84b69c36526a472c72fa5f2229000ab88
Miliexe <emilie.lblc@gmail.com>
2022-12-01 00:38:18 +0100

    Init Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Routes from './Routes'
+
+import './styles/index.css'
+
+function App() {
+    return (
+        <div className="App">
+            <Routes />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Routes.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import Home from './pages/Home'
+import Error404 from './pages/Error404'
+
+const index = () => {
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/*" element={<Error404 />} />
+            </Routes>
+            <Footer />
+        </Router>
+    )
+}
+
+export default index
