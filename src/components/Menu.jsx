624317419c19bcc5864333dcb7ab2d133ca14d12
Miliexe <emilie.lblc@gmail.com>
2023-01-29 01:21:17 +0100

    Update Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hamburger from 'hamburger-react'
 import { useState } from 'react'
-import Nav from './Nav'
 
 const Menu = () => {
     const [stateCollapse, setStateCollapse] = useState(0)
@@ -15,7 +14,69 @@ const Menu = () => {
                     stateCollapseUpdate()
                 }}
             />
-            {stateCollapse ? <div className="menu--open"></div> : null}
+            {stateCollapse ? (
+                <div>
+                    <div className="menu--open"></div>
+                    <div className="menu__content">
+                        <ul>
+                            <li>
+                                <a
+                                    href="https://github.com/Miliexe/Kasa#readme"
+                                    target={'_blank'}
+                                    rel="noreferrer"
+                                >
+                                    Kasa
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/Miliexe/Piiquante#readme"
+                                    target={'_blank'}
+                                    rel="noreferrer"
+                                >
+                                    Piiquante
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/Miliexe/Kanap#readme"
+                                    target={'_blank'}
+                                    rel="noreferrer"
+                                >
+                                    Kanap
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/Miliexe/Panthere_after#readme"
+                                    target={'_blank'}
+                                    rel="noreferrer"
+                                >
+                                    La Panthère
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/Miliexe/Ohmyfood#readme"
+                                    target={'_blank'}
+                                    rel="noreferrer"
+                                >
+                                    Ohmyfood
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="https://github.com/Miliexe/Booki#readme"
+                                    target={'_blank'}
+                                    rel="noreferrer"
+                                >
+                                    Booki
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            ) : null}
         </div>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,12 +11,6 @@ const Nav = () => {
                 >
                     <li>Accueil</li>
                 </NavLink>
-                <NavLink
-                    to="/about"
-                    className={(nav) => (nav.isActive ? 'nav--active' : '')}
-                >
-                    <li>A Propos</li>
-                </NavLink>
             </ul>
         </div>
     )
