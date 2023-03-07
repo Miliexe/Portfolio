54a8ffdf4188f6e8cf0523f55aa93b9f58c57df8
Miliexe <emilie.lblc@gmail.com>
2023-03-07 22:42:42 +0100

    Init Avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,95 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import myAvatar from '../asset/avatar.glb'
+
+export function Model(props) {
+    const { nodes, materials } = useGLTF(myAvatar)
+    return (
+        <group {...props} dispose={null}>
+            <primitive object={nodes.Hips} />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Body.geometry}
+                material={materials.Wolf3D_Body}
+                skeleton={nodes.Wolf3D_Body.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+                material={materials.Wolf3D_Outfit_Bottom}
+                skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+                material={materials.Wolf3D_Outfit_Footwear}
+                skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Outfit_Top.geometry}
+                material={materials.Wolf3D_Outfit_Top}
+                skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+            />
+            <skinnedMesh
+                geometry={nodes.Wolf3D_Hair.geometry}
+                material={materials.Wolf3D_Hair}
+                skeleton={nodes.Wolf3D_Hair.skeleton}
+            />
+            <skinnedMesh
+                name="EyeLeft"
+                geometry={nodes.EyeLeft.geometry}
+                material={materials.Wolf3D_Eye}
+                skeleton={nodes.EyeLeft.skeleton}
+                morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+                morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="EyeRight"
+                geometry={nodes.EyeRight.geometry}
+                material={materials.Wolf3D_Eye}
+                skeleton={nodes.EyeRight.skeleton}
+                morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+                morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="Wolf3D_Head"
+                geometry={nodes.Wolf3D_Head.geometry}
+                material={materials.Wolf3D_Skin}
+                skeleton={nodes.Wolf3D_Head.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+            />
+            <skinnedMesh
+                name="Wolf3D_Teeth"
+                geometry={nodes.Wolf3D_Teeth.geometry}
+                material={materials.Wolf3D_Teeth}
+                skeleton={nodes.Wolf3D_Teeth.skeleton}
+                morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+                morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+            />
+        </group>
+    )
+}
+
+function Avatar() {
+    return (
+        <div className="avatar">
+            <Canvas
+                className="avatar-3d"
+                camera={{ fov: 3, position: [0, 0, 90] }}
+            >
+                <Suspense fallback={null}>
+                    <ambientLight />
+                    <directionalLight intensity={2} position={[0, 0, 50]} />
+                    <Model />
+                    <OrbitControls
+                        autoRotate={true}
+                        enablePan={false}
+                        enableZoom={false}
+                        enableRotate={true}
+                    />
+                </Suspense>
+            </Canvas>
+        </div>
+    )
+}
+
+export default Avatar
