c86c8cb892ba033532a4919473b82058f2e99759
Miliexe <emilie.lblc@gmail.com>
2023-03-10 06:02:16 +0100

    Init Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import github from '../asset/github.svg'
+import mail from '../asset/mail.png'
+import linkedin from '../asset/linkedin.svg'
+
+const Profile = () => {
+    return (
+        <div className="profile">
+            <h2 className="profile__title">Player: Miliexe</h2>
+            <span className="profile__description">Web Developper</span>
+            <div className="profile__content">
+                <ul className="profile__section">
+                    <h3>Skills:</h3>
+                    <li>CSS</li>
+                    <li>SASS</li>
+                    <li>HTML5</li>
+                    <li>React</li>
+                    <li>Javascript</li>
+                </ul>
+                <ul className="profile__section">
+                    <h3>Level:</h3>
+                    <li>Junior</li>
+                    <li>Bac+2</li>
+                </ul>
+                <ul className="profile__section flex-end">
+                    <li>
+                        <a
+                            href="https://github.com/Miliexe"
+                            target={'_blank'}
+                            rel="noreferrer"
+                        >
+                            <img src={github} alt="GitHub Logo" />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/leblanc-emilie/"
+                            target={'_blank'}
+                            rel="noreferrer"
+                        >
+                            <img src={linkedin} alt="Linkedin Logo" />
+                        </a>
+                        <a href="mailto:emilie.lblc@gmail.com">
+                            <img src={mail} alt="Email Logo" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Avatar from '../components/Avatar'
+import Profile from '../components/Profile'
 
 const Home = () => {
     return (
@@ -7,6 +8,7 @@ const Home = () => {
             <div className="avatar-container">
                 <Avatar></Avatar>
             </div>
+            <Profile></Profile>
         </div>
     )
 }
