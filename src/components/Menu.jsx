34d9929eae9abc4f21edaf1df6ecd59434d975df
Miliexe <emilie.lblc@gmail.com>
2023-03-13 00:37:09 +0100

    Update Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,77 +10,95 @@ const Menu = () => {
     }
     return (
         <div className="menu">
-            <Hamburger
-                onToggle={() => {
-                    stateCollapseUpdate()
-                }}
-            />
+            <Hamburger toggled={stateCollapse} toggle={stateCollapseUpdate} />
             {stateCollapse ? (
                 <div className="menu--open">
-                    <div className="menu__body"></div>
-                    <div className="menu__content">
-                        <span>Mes réalisations:</span>
-                        <ul>
-                            <NavLink
-                                to="/project/Kasa"
-                                className={(nav) =>
-                                    nav.isActive ? 'menu--active' : ''
-                                }
-                            >
-                                <li>Kasa</li>
-                            </NavLink>
-                            <NavLink
-                                to="/project/Piiquante"
-                                className={(nav) =>
-                                    nav.isActive ? 'menu--active' : ''
-                                }
-                            >
-                                <li>Piiquante</li>
-                            </NavLink>
-                            <NavLink
-                                to="/project/Kanap"
-                                className={(nav) =>
-                                    nav.isActive ? 'menu--active' : ''
-                                }
-                            >
-                                <li>Kanap</li>
-                            </NavLink>
-                            <NavLink
-                                to="/project/La-Panthere"
-                                className={(nav) =>
-                                    nav.isActive ? 'menu--active' : ''
-                                }
-                            >
-                                <li>La Panthère</li>
-                            </NavLink>
-                            <NavLink
-                                to="/project/Ohmyfood"
-                                className={(nav) =>
-                                    nav.isActive ? 'menu--active' : ''
-                                }
-                            >
-                                <li>Ohmyfood</li>
-                            </NavLink>
-                            <NavLink
-                                to="/project/Booki"
-                                className={(nav) =>
-                                    nav.isActive ? 'menu--active' : ''
-                                }
-                            >
-                                <li>Booki</li>
-                            </NavLink>
-                        </ul>
-                        <ul>
-                            <NavLink
-                                to="/Error404"
-                                className={(nav) =>
-                                    nav.isActive ? 'menu--active' : ''
-                                }
-                            >
-                                <br />
-                                <li>404 Not Found</li>
-                            </NavLink>
-                        </ul>
+                    <div className="menu__body">
+                        <div className="menu__content">
+                            <span>Mes réalisations:</span>
+                            <ul>
+                                <NavLink
+                                    to="/project/Kasa"
+                                    className={(nav) =>
+                                        nav.isActive ? 'menu--active' : ''
+                                    }
+                                    onClick={() =>
+                                        setStateCollapse(!stateCollapse)
+                                    }
+                                >
+                                    <li>Kasa</li>
+                                </NavLink>
+                                <NavLink
+                                    to="/project/Piiquante"
+                                    className={(nav) =>
+                                        nav.isActive ? 'menu--active' : ''
+                                    }
+                                    onClick={() =>
+                                        setStateCollapse(!stateCollapse)
+                                    }
+                                >
+                                    <li>Piiquante</li>
+                                </NavLink>
+                                <NavLink
+                                    to="/project/Kanap"
+                                    className={(nav) =>
+                                        nav.isActive ? 'menu--active' : ''
+                                    }
+                                    onClick={() =>
+                                        setStateCollapse(!stateCollapse)
+                                    }
+                                >
+                                    <li>Kanap</li>
+                                </NavLink>
+                                <NavLink
+                                    to="/project/La-Panthere"
+                                    className={(nav) =>
+                                        nav.isActive ? 'menu--active' : ''
+                                    }
+                                    onClick={() =>
+                                        setStateCollapse(!stateCollapse)
+                                    }
+                                >
+                                    <li>La Panthère</li>
+                                </NavLink>
+                                <NavLink
+                                    to="/project/Ohmyfood"
+                                    className={(nav) =>
+                                        nav.isActive ? 'menu--active' : ''
+                                    }
+                                    onClick={() =>
+                                        setStateCollapse(!stateCollapse)
+                                    }
+                                >
+                                    <li>Ohmyfood</li>
+                                </NavLink>
+                                <NavLink
+                                    to="/project/Booki"
+                                    className={(nav) =>
+                                        nav.isActive ? 'menu--active' : ''
+                                    }
+                                    onClick={() =>
+                                        setStateCollapse(!stateCollapse)
+                                    }
+                                >
+                                    <li>Booki</li>
+                                </NavLink>
+                            </ul>
+                            <ul>
+                                <NavLink
+                                    to="/Error404"
+                                    className={(nav) =>
+                                        nav.isActive ? 'menu--active' : ''
+                                    }
+                                    onClick={() =>
+                                        setStateCollapse(!stateCollapse)
+                                    }
+                                >
+                                    <br />
+                                    <li>404 Not Found</li>
+                                </NavLink>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             ) : null}
